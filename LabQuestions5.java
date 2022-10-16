0b63ae7db2fccdb48914f701c7e62222a3b57894
SHJA AZIMI <110715621+SHJAx19@users.noreply.github.com>
2022-10-16 13:48:27 +0300

    Add files via upload

---
+++ LabQuestions5.java
@@ -0,0 +1,20 @@
+
+package labquestions5;
+import java.util.Scanner;
+public class LabQuestions5 {
+
+    public static void main(String[] args) {
+  
+        Scanner money = new Scanner(System.in);
+        
+        System.out.println("Please enter the amount of TL :");
+        double tl = money.nextDouble();
+        
+        
+        
+        System.out.println(tl+"  TL is equal to 5 10 TL, 1 5 TL and 1 1 TL.");
+        
+        
+    }
+    
+}

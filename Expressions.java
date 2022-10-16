51219e456bea73a0f72f5fcef08cb106db663035
SHJA AZIMI <110715621+SHJAx19@users.noreply.github.com>
2022-10-16 12:01:05 +0300

    Add files via upload

---
+++ Expressions.java
@@ -0,0 +1,24 @@
+package expressions;
+
+public class Expressions {
+
+    public static void main(String[] args) {
+
+        //Expressions = Operands & Operators
+        
+        
+        int friends = 20;
+        
+        friends++;
+        
+        System.out.println(friends);
+        
+       
+        
+        
+        
+       
+    }
+    
+}
+  

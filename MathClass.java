9ed809d5f8a4b84a72c25e577aea8cd292b9af35
SHJA AZIMI <110715621+SHJAx19@users.noreply.github.com>
2022-10-16 13:00:47 +0300

    Add files via upload

---
+++ MathClass.java
@@ -0,0 +1,23 @@
+
+package math.pkgclass;
+
+
+public class MathClass {
+
+  
+    public static void main(String[] args) {
+ 
+        double x;
+        double y;
+        double z;
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("enter side x:");
+        x = scanner.nextDouble();
+        System.out.println("Enter siide y:");
+        y = scanner.nextDouble();
+        
+    }
+    
+}

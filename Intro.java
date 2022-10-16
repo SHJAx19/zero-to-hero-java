ddb4f4c758044eed7df51ff7c0839b89e5f31914
SHJA AZIMI <110715621+SHJAx19@users.noreply.github.com>
2022-10-16 11:58:21 +0300

    Add files via upload

---
+++ Intro.java
@@ -0,0 +1,16 @@
+
+package intro;
+
+public class Intro {
+
+    public static void main(String[] args) {
+    
+        System.out.println("Hello Iam lerning Java");
+        System.out.println("I am 1st year student of Computer Engineering");
+        System.out.println("If you find it helpful please remember to follow me. ");
+        System.out.println("Thanks for support");
+        
+        
+    }
+    
+}
---
+++ Variables.java
@@ -0,0 +1,21 @@
+
+package intro;
+
+public class Intro {
+
+    public static void main(String[] args) {
+        
+        // Variables = a placeholder for a value that behaves as the value it contains. 
+ 
+        int x = 32;
+        double y = 3.21; 
+        boolean t = false; 
+        char z = '@';
+        
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(t);
+        System.out.println(z);
+    }
+    
+}
